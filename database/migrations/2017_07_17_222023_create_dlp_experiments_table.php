92fda7da93dad8f8158a77231f9e8a707b00efab
Danny Ng <dannynsl@gmail.com>
2017-07-17 15:59:52 -0700

    - Updated `.env.example` file - Modified User migrations table to reflect production tables. - Added DLP Experiments migrations table.

---
+++ database/migrations/2017_07_17_222023_create_dlp_experiments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDlpExperimentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dlp_experiments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name')->nullable();
+            $table->json('rules')->nullable();
+            $table->json('options');
+            $table->timestamp('expiration')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dlp_experiments');
+    }
+}
