445b517407e97859d82f1217d2e545cf81659fdd
Danny Ng <dannynsl@gmail.com>
2017-07-26 11:51:56 -0700

    - Improved `getImagePath()` to utilize `$request->secure()` to check for protocol. - Added method comments. - Cleaned up spacing and parantheses style. - Added error response for unset `domain_url` attribute for user. - Changed error code and message for accessing unauthorized experiment.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,12 +16,26 @@ class ApiController extends Controller
 
     }
 
-    private function getImagePath($filename){
-        $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
+    /**
+     * Return full path of filename.
+     *
+     * @param   string  $filename  
+     * @return  string             
+     */
+    private function getImagePath($filename) {
+        $protocol = request()->secure() ? 'https://' : 'http://';
         return $protocol.$_SERVER['SERVER_NAME'].'/'.env('UPLOAD_DIR').'/'.$filename;
     }
 
-    private function checkRule($str1, $str2, $operator){
+    /**
+     * Check experiment rules to see if its valid to run. 
+     *
+     * @param   string  $str1      Variable name.
+     * @param   string  $str2      Value to check against.
+     * @param   string  $operator  Comparison operator.
+     * @return  bool             
+     */
+    private function checkRule($str1, $str2, $operator): bool {
         $valid = 0;
         switch ($operator) {
             case 'contain':
@@ -43,34 +57,34 @@ class ApiController extends Controller
         return $valid;
     }
 
+    /**
+     * API call to run experiment.
+     *
+     * @return  Response  
+     */
     public function getExpInfo()
     {
         header('content-type: application/json; charset=utf-8');
         header("access-control-allow-origin: *");
 
-
+        // Retrieve parameters
         $fullName = $_POST['fullName'];
         $pathName = $_POST['pathName'];
         $domainName = $_POST['domainName'];
-        $is_valid = 0;
-
         $querystring = parse_url($fullName, PHP_URL_QUERY);
         parse_str($querystring, $vars);
         $exp_id = isset($vars['exp_id']) ? $vars['exp_id'] : null ;
 
+        $is_valid = 0;
         $experiment = Experiments::with('user')->find($exp_id);
-
-        if($experiment)
-        {
+        if ($experiment) {
             if (isset($experiment->user->domain_url)) {
                 if ($experiment->user->domain_url === $domainName) {
                     $rules = json_decode($experiment->rules);
                     $options = json_decode($experiment->options);
                     $is_valid = 1;
-                    if($rules != '' && isset($rules->variable))
-                    {
-                        switch($rules->variable)
-                        {
+                    if ($rules != '' && isset($rules->variable)) {
+                        switch ($rules->variable) {
                             case 'domain':
                                 $is_valid = self::checkRule($domainName,$rules->value, $rules->operator);
                                 break;
@@ -78,30 +92,29 @@ class ApiController extends Controller
                                 $is_valid = self::checkRule($pathName,$rules->value, $rules->operator);
                                 break;
                             default:
-                                $is_valid =0;
+                                $is_valid = 0;
                                 break;
                         }
                     }
-                    if($is_valid)
-                    {
-                        foreach($options as &$item)
-                        {
-                            if($item->type == 'image')
-                            {
+                    if ($is_valid) {
+                        foreach ($options as &$item) {
+                            if ($item->type == 'image') {
                                 $item->value = self::getImagePath($item->value);
                             }
                         }
                         return response()->success($options);
-                    }else{
+                    } else {
                         // Rule doesn't match
-                        return response()->error('Rule not match', 405);
+                        return response()->error('Mismatched rule', 403);
                     }
                 } else {
                     // User is accessing experiment that doesn't belong to user
-                    return response()->error('Forbidden', 403);
+                    return response()->error('Unauthorized', 401);
                 }
+            } else {
+                return response()->error('Check domain url', 403);
             }
-        }else{
+        } else {
             // No experiment found
             return response()->error('Not found', 404);
         }
