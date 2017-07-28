a29493c868a76088c9c1f54b135691ad5847e96d
Danny Ng <dannynsl@gmail.com>
2017-07-28 15:56:33 -0700

    Further testing.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -26,12 +26,11 @@ class ApiController extends Controller
         // Heroku passes X-Forwarded-Proto in the headers. 
         // See https://devcenter.heroku.com/articles/http-routing#heroku-headers
         if (env('APP_ENV') === 'production') {
-            //$protocol = ($request->header('x-forwarded-proto') == 'https') ? 'https://' : 'http://';
+            $protocol = (request()->header('x-forwarded-proto') == 'https') ? 'https://' : 'http://';
         } else {
             $protocol = request()->secure() ? 'https://' : 'http://';
         }
-        $protocol = 'https://';
-        //echo $request->header('x-forwarded-proto');
+
         return $protocol.$_SERVER['SERVER_NAME'].'/'.env('UPLOAD_DIR').'/'.$filename;
     }
 
